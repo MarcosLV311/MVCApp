eac69e17ef502a8f1bd555d82aa87e8d48aa73ff
MarcosLV <marcoslaguna311@gmail.com>
2024-03-09 09:49:01 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Welcome(string name, int numTimes = 1)
+		{ 
+			ViewData["Message"] = "Hello " + name;
+			ViewData["NumTimes"] = numTimes;
+			return View();
+		}
+
+		//GET: /HelloWorld/
+		//public string Index()
+		//{
+		//    return "This is a Index action...";
+		//}
+
+		//GET: /HelloWorld/Welcome/
+		//Requires using System.Text.Encodings.Web
+		//public string Welcome(string name, int numTimes = 1)
+		//{
+		//    //return "This is a Index action...";
+		//    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+		//}
+	}
+}
---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+namespace MVCApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCAppContext>>()))
+            {
+                // Look all movies
+                if (context.Movie.Any())
+                {
+                    return; // DB has been seeded   
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Haryry meet Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbuster",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "PG-13"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbuster 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "PG-13"
+                    }, new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
